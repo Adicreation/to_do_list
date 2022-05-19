04080c5471e61de346c3d06ae47b80b5a4344f04
Adicreation <100214935+Adicreation@users.noreply.github.com>
2022-05-19 23:26:47 +0530

    db changes

--- settings.py
+++ settings.py
@@ -85,12 +85,27 @@ WSGI_APPLICATION = 'to_do_list.wsgi.application'
 #     }
 # }
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todolist',
+        'USER': 'aditya',
+        'PASSWORD': 'Aditya@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
+
 
 
 # Password validation
@@ -135,6 +150,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR/'media/')
+
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles/')
 
 STATICFILES_DIRS = [
     BASE_DIR /'static'
